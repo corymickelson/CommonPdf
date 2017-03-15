221dc7163a99f9ed5486df626c4d7c4173244736
Cory Mickelson <cmickelson@skyslope.com>
2017-03-15 11:55:21 -0700

    Add form splitting option to concat

--- src/concat.spec.js
+++ src/concat.spec.js
@@ -25,4 +25,58 @@ test( 'Concat', t => {
 			t.fail()
 		} )
 } )
-
+test( 'Split', t => {
+	new Subject( [ join( __dirname, '../test-data/fw9.pdf' ) ], null, [ {
+		start: 1,
+		end: 2
+	}, {
+		start: 4,
+		end: 'end'
+	} ] )
+		.write()
+		.then( outFile => {
+			t.plan( 1 )
+			exec( `pdftk ${outFile} dump_data | grep -i NumberOfPages`, ( error, stdin, stderr ) => {
+				if( error || stderr ) t.fail()
+				else {
+					t.equal( stdin[ stdin.indexOf( ':' ) + 2 ], '3' )
+				}
+			} )
+		} )
+		.catch( e => {
+			t.fail()
+		} )
+} )
+test( 'Split 1-1', t => {
+	new Subject( [ join( __dirname, '../test-data/fw9.pdf' ) ], null, [ {
+		start: 1,
+		end: 1
+	}, {
+		start: 4,
+		end: 'end'
+	} ] )
+		.write()
+		.then( outFile => {
+			t.plan( 1 )
+			exec( `pdftk ${outFile} dump_data | grep -i NumberOfPages`, ( error, stdin, stderr ) => {
+				if( error || stderr ) t.fail()
+				else {
+					t.equal( stdin[ stdin.indexOf( ':' ) + 2 ], '2' )
+				}
+			} )
+		} )
+		.catch( e => {
+			t.fail()
+		} )
+} )
+test( 'Concat and split', t => {
+	t.plan( 1 )
+	t.equal( new Subject( [ join( __dirname, '../test-data/fw9.pdf' ), join( __dirname, '../test-data/de542.pdf' ) ], null, [ {
+			start: 1,
+			end: 1
+		}, {
+			start: 4,
+			end: 'end'
+		} ] ).message,
+		'Can not concat and split. Try, concatenating first, and splitting afterwards.' )
+} )
