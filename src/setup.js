ad958c11c7976deab63f483cfe6f7985f6aa2726
Cory Mickelson <cmickelson@skyslope.com>
2017-08-11 11:45:29 -0700

    remove node_modules check on setup, bump version

--- src/setup.js
+++ src/setup.js
@@ -27,9 +27,8 @@ function findCommonPdfBinaries() {
 }
 function setup() {
     return __awaiter(this, void 0, void 0, function* () {
-        const check = yield findCommonPdfBinaries();
-        if (!check)
-            throw Error('CommonPdf binaries not found!');
+        //const check = await findCommonPdfBinaries()
+        //if (!check) throw Error('CommonPdf binaries not found!')
         const commonPdfPodofo = `${process.env['LAMBDA_TASK_ROOT']}/node_modules/commonpdf_podofo`;
         const commonPdfPdftk = `${process.env['LAMBDA_TASK_ROOT']}/node_modules/commonpdf_pdftk`;
         process.env['PATH'] = `${process.env['PATH']}:${commonPdfPdftk}:${commonPdfPodofo}`;
--- src/setup.ts
+++ src/setup.ts
@@ -16,8 +16,8 @@ async function findCommonPdfBinaries(): Promise<boolean> {
 }
 
 export async function setup(): Promise<void> {
-    const check = await findCommonPdfBinaries()
-    if (!check) throw Error('CommonPdf binaries not found!')
+    //const check = await findCommonPdfBinaries()
+    //if (!check) throw Error('CommonPdf binaries not found!')
     const commonPdfPodofo = `${process.env['LAMBDA_TASK_ROOT']}/node_modules/commonpdf_podofo`
     const commonPdfPdftk = `${process.env['LAMBDA_TASK_ROOT']}/node_modules/commonpdf_pdftk`
     process.env['PATH'] = `${process.env['PATH']}:${commonPdfPdftk}:${commonPdfPodofo}`
